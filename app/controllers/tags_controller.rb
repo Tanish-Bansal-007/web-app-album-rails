994a1761d6fc9deec17d0b2a42128cbbe1944a0a
Tanish Bansal <tbansal@anviam.com>
2021-04-22 12:51:53 +0530

    entering multiple tags and showing tags

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -6,15 +6,13 @@ class TagsController < ApplicationController
     def create
         @album = current_user.albums.find(params[:album_id])
         image = params[:image]
-        tags = params[:tag][:tag_name]
-        tags.each do |tag|
-            if tag.present?
-                @tag = Tag.new(tag_name: tag, image_id: image)
-                @tag.save
-                redirect_to albums_path
-            else
-                redirect_to albums_path
-            end
+        tag = params[:tag][:tag_name]
+        if tag.present?
+            @tag = Tag.new(tag_name: tag, image_id: image)
+            @tag.save
+            redirect_to album_path(@album)
+        else
+            redirect_to album_path(@album)
         end
     end    
 end
