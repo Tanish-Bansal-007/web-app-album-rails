dba7a5537898eecd6f1269195cf1ef77609d27a9
Tanish Bansal <tbansal@anviam.com>
2021-04-23 10:07:09 +0530

    Showing tags on modal (pending)

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -5,14 +5,17 @@ class TagsController < ApplicationController
 
     def create
         @album = current_user.albums.find(params[:album_id])
-        image = params[:image]
-        tag = params[:tag][:tag_name]
-        if tag.present?
-            @tag = Tag.new(tag_name: tag, image_id: image)
-            @tag.save
-            redirect_to album_path(@album)
-        else
-            redirect_to album_path(@album)
+        image_id = params[:image]
+        tags = params[:tag][:tag_name].split(",")
+        tags.each do |tag|
+            tag = Tag.new(tag_name: tag, image_id: image_id)
+            tag.save
         end
+        redirect_to album_path(@album)
+        # if tag.present?
+            
+        # else
+        #     redirect_to album_path(@album)
+        # end
     end    
 end
